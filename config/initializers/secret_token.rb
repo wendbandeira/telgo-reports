093b1c56b1e16d204773ad254c3ba9afd4c7cae1
wendbandeira <wendbandeira@gmail.com>
2014-10-02 17:47:59 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TelgoReports::Application.config.secret_key_base = '4f7cb8f5c58dac5780425e1430c8925d52fb896a8531516e08cbb0278b7d545bedbb425352f73c8ba9d3e313b60b68224ead81a57f61eb0e532ecf7e71fb1f9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TelgoReports::Application.config.session_store :cookie_store, key: '_telgo-reports_session'
