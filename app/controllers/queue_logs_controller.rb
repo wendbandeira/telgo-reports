76d3a342b4b927a04093bc07c74759407ec0b614
wendbandeira <wendbandeira@gmail.com>
2014-10-09 16:54:23 -0300

    Correções de dados de listagem

--- app/controllers/queue_logs_controller.rb
+++ app/controllers/queue_logs_controller.rb
@@ -1,7 +1,7 @@
 class QueueLogsController < ApplicationController
 
   def index
-    @queue_logs = QueueLog.events.paginate(page: params[:page], per_page: 20 )
+    @queue_logs = QueueLog.events.paginate(page: params[:page], per_page: 10 )
   end
 
   def show
