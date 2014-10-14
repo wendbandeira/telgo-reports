c031257c117d1fe8fcbf94c4022729fecf052651
wendbandeira <wendbandeira@gmail.com>
2014-10-14 15:08:18 -0300

    Implantação de busca no sistema

--- app/controllers/queue_logs_controller.rb
+++ app/controllers/queue_logs_controller.rb
@@ -1,7 +1,9 @@
 class QueueLogsController < ApplicationController
 
   def index
-    @queue_logs = QueueLog.events.paginate(page: params[:page], per_page: 10 )
+    @queue_logs = QueueLog.events.paginate(page: params[:page], per_page:10)
+    @queue_logs = QueueLog.search(params[:datainicial], params[:datafinal]).paginate(page: params[:page],
+    per_page: 10) if params[:datainicial].present?
   end
 
   def show
